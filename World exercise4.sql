da2b789cf5c49e5ac4be2dcc5832a35d75e56587
Poojagurrapu7890 <pooja.gurrapu@mobiuso.com>
2025-01-15 20:44:31 +0530

    Add files via upload

---
+++ World exercise4.sql
@@ -0,0 +1,25 @@
+SHOW TABLES;
+DESCRIBE sakila.actor;
+SHOW CREATE TABLE sakila.actor;
+SELECT first_name, last_name FROM sakila.actor;
+
+SELECT first_name, last_name FROM sakila.actor WHERE last_name = 'Johansson';
+SELECT UPPER(CONCAT(first_name, ' ', last_name)) AS "Actor Name" FROM sakila.actor;
+SELECT actor_id, first_name, last_name FROM sakila.actor WHERE first_name = 'Joe';
+SELECT last_name FROM sakila.actor GROUP BY last_name HAVING COUNT(*) = 1;
+SELECT staff.first_name, staff.last_name, address.address 
+FROM staff 
+JOIN address ON staff.address_id = address.address_id;
+
+-- WORLD DATABASE EXERCISE
+SELECT * FROM city LIMIT 10;
+SELECT * FROM city LIMIT 5 OFFSET 15;
+SELECT COUNT(*) AS total_rows FROM city;
+SELECT name, population 
+FROM city 
+ORDER BY population DESC 
+LIMIT 1;
+
+
+
+
