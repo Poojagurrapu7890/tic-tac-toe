559beb1243b0ab4d3056b5f97a3c5ec14dce6227
Poojagurrapu7890 <pooja.gurrapu@mobiuso.com>
2025-01-20 11:00:44 +0530

    sqlite ex-1 question 2.sql

--- sqlite ex 1.sql
+++ sqlite ex 1.sql
@@ -1,3 +1,7 @@
 SELECT Title
 FROM Albums
-WHERE AlbumId = '67'
+WHERE AlbumId = '67';
+
+SELECT Name, Milliseconds / 1000 AS LengthInSeconds
+FROM tracks
+WHERE Milliseconds BETWEEN 50000 AND 70000;
