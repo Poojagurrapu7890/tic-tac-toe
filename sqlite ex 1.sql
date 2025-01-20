386e47c2a37ac4ae1b817fe9e5001581d0fe087e
Poojagurrapu7890 <pooja.gurrapu@mobiuso.com>
2025-01-20 11:08:16 +0530

    sqlite ex-1 question 3.sql

--- sqlite ex 1.sql
+++ sqlite ex 1.sql
@@ -5,3 +5,8 @@ WHERE AlbumId = '67';
 SELECT Name, Milliseconds / 1000 AS LengthInSeconds
 FROM tracks
 WHERE Milliseconds BETWEEN 50000 AND 70000;
+
+SELECT Title, Name
+FROM albums
+JOIN artists ON albums.ArtistId = artists.ArtistId
+WHERE Name LIKE '%black%';
